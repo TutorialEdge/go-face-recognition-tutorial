14d5d901d06138ed905b46bf4d04840872766106
Elliot Forbes <emforce@live.com>
2018-09-28 13:22:11 +0100

    Updates

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/TutorialEdge/go-face-recognition-tutorial
+
+require (
+	github.com/Kagami/go-face v0.0.0-20180922080525-863b26a6405e
+	github.com/esimov/pigo v1.0.3 // indirect
+	github.com/fogleman/gg v1.1.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.0.0-20180926015637-991ec62608f3 // indirect
+)
---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+
+	"github.com/Kagami/go-face"
+)
+
+const dataDir = "testdata"
+
+func main() {
+	fmt.Println("Facial Recognition System v0.01")
+
+	rec, err := face.NewRecognizer(dataDir)
+	if err != nil {
+		fmt.Println("Cannot initialize recognizer")
+	}
+	defer rec.Close()
+
+	fmt.Println("Recognizer Initialized")
+
+	avengersImage := filepath.Join(dataDir, "avengers-02.jpeg")
+
+	faces, err := rec.RecognizeFile(avengersImage)
+	if err != nil {
+		log.Fatalf("Can't recognize: %v", err)
+	}
+	fmt.Println("Number of Faces in Image: ", len(faces))
+
+	var samples []face.Descriptor
+	var avengers []int32
+	for i, f := range faces {
+		samples = append(samples, f.Descriptor)
+		// Each face is unique on that image so goes to its own category.
+		avengers = append(avengers, int32(i))
+	}
+	// Name the categories, i.e. people on the image.
+	labels := []string{
+		"Dr Strange",
+		"Tony Stark",
+		"Bruce Banner",
+		"Wong",
+	}
+	// Pass samples to the recognizer.
+	rec.SetSamples(samples, avengers)
+
+	// Now let's try to classify some not yet known image.
+	testTonyStark := filepath.Join(dataDir, "tony-stark.jpg")
+	tonyStark, err := rec.RecognizeSingleFile(testTonyStark)
+	if err != nil {
+		log.Fatalf("Can't recognize: %v", err)
+	}
+	if tonyStark == nil {
+		log.Fatalf("Not a single face on the image")
+	}
+	avengerID := rec.Classify(tonyStark.Descriptor)
+	if avengerID < 0 {
+		log.Fatalf("Can't classify")
+	}
+
+	fmt.Println(avengerID)
+	fmt.Println(labels[avengerID])
+
+	testDrStrange := filepath.Join(dataDir, "dr-strange.jpg")
+	drStrange, err := rec.RecognizeSingleFile(testDrStrange)
+	if err != nil {
+		log.Fatalf("Can't recognize: %v", err)
+	}
+	if drStrange == nil {
+		log.Fatalf("Not a single face on the image")
+	}
+	avengerID = rec.Classify(drStrange.Descriptor)
+	if avengerID < 0 {
+		log.Fatalf("Can't classify")
+	}
+
+	fmt.Println(avengerID)
+	fmt.Println(labels[avengerID])
+
+	testWong := filepath.Join(dataDir, "wong.jpg")
+	wong, err := rec.RecognizeSingleFile(testWong)
+	if err != nil {
+		log.Fatalf("Can't recognize: %v", err)
+	}
+	if wong == nil {
+		log.Fatalf("Not a single face on the image")
+	}
+	avengerID = rec.Classify(wong.Descriptor)
+	if avengerID < 0 {
+		log.Fatalf("Can't classify")
+	}
+	fmt.Println(avengerID)
+	fmt.Println(labels[avengerID])
+
+}
